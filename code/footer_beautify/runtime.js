d984b2840fae9116705b68bae1e592f89d266c28
Lanlinan <3572066061@qq.com>
2022-12-07 16:03:25 +0800

    npm publish

---
+++ code/footer_beautify/runtime.js
@@ -0,0 +1,36 @@
+setInterval(() => {
+  let create_time = Math.round(new Date('2021-09-09 00:00:00').getTime() / 1000); //在此行修改建站时间
+  let timestamp = Math.round((new Date().getTime()) / 1000);
+  let second = timestamp - create_time;
+  let time = new Array(0, 0, 0, 0, 0);
+
+  var nol = function(h){
+    return h>9?h:'0'+h;
+  }
+  if (second >= 365 * 24 * 3600) {
+    time[0] = parseInt(second / (365 * 24 * 3600));
+    second %= 365 * 24 * 3600;
+  }
+  if (second >= 24 * 3600) {
+    time[1] = parseInt(second / (24 * 3600));
+    second %= 24 * 3600;
+  }
+  if (second >= 3600) {
+    time[2] = nol(parseInt(second / 3600));
+    second %= 3600;
+  }
+  if (second >= 60) {
+    time[3] = nol(parseInt(second / 60));
+    second %= 60;
+  }
+  if (second > 0) {
+    time[4] = nol(second);
+  }
+  if ((Number(time[2])<22) && (Number(time[2])>7)){
+    currentTimeHtml ="<img class='boardsign' src='https://npm.elemecdn.com/ylan-blog/images/footer/sfz-6adea8.svg' title='白天摸鱼，不过是为了晚上好好加班罢了~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+  }
+  else{
+    currentTimeHtml ="<img class='boardsign' src='https://npm.elemecdn.com/ylan-blog/images/footer/503-6adea8.svg' title='好好熬夜，才能在白天好好的摸鱼'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+  }
+  document.getElementById("workboard").innerHTML = currentTimeHtml;
+}, 1000);
